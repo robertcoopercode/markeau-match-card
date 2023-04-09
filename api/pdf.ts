b218bc4a42a2f4f6b5725b55dffc17af713580bc
Robert Cooper <robert@robertcooper.me>
2023-04-09 13:16:01 -0400

    Try using browserless service :)

--- api/pdf.ts
+++ api/pdf.ts
@@ -1,48 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import chromium from 'chrome-aws-lambda';
 import puppeteer from 'puppeteer-core';
-// import { z, ZodError } from "zod";
+import { z, ZodError } from "zod";
 
-// const expectedBody = z.object({
-// 	divisionName: z.string(),
-// 	formattedDate: z.string().optional(),
-// 	matchNumber: z.string().optional(),
-// 	fieldName: z.string().optional(),
-// 	currentTeamName: z.string(),
-// 	homeTeamName: z.string().optional(),
-// 	awayTeamName: z.string().optional(),
-// 	teamPlayers: z.array(
-// 		z.object({
-// 			number: z.number().nullable(),
-// 			first_name: z.string(),
-// 			last_name: z.string(),
-// 			reserve: z.boolean(),
-// 			suspended: z.boolean().optional(),
-// 		})
-// 	)
-// })
-//
-// type ExpectedBody = z.infer<typeof expectedBody>;
+const expectedBody = z.object({
+	divisionName: z.string(),
+	formattedDate: z.string().optional(),
+	matchNumber: z.string().optional(),
+	fieldName: z.string().optional(),
+	currentTeamName: z.string(),
+	homeTeamName: z.string().optional(),
+	awayTeamName: z.string().optional(),
+	teamPlayers: z.array(
+		z.object({
+			number: z.number().nullable(),
+			first_name: z.string(),
+			last_name: z.string(),
+			reserve: z.boolean(),
+			suspended: z.boolean().optional(),
+		})
+	)
+})
+
+type ExpectedBody = z.infer<typeof expectedBody>;
 
 const generatePdf = async (html = '') => {
-	const options = process.env.AWS_REGION
-		? {
-				args: chromium.args,
-				executablePath: await chromium.executablePath,
-				headless: chromium.headless,
-		  }
-		: {
-				args: [],
-				executablePath:
-					process.platform === 'win32'
-						? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-						: process.platform === 'linux'
-						? '/usr/bin/google-chrome'
-						: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-		  };
-
-	const browser = await puppeteer.launch(options);
+	const browser = await puppeteer.connect({
+		browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_TOKEN}`,
+	});
 	const page = await browser.newPage();
 
 	await page.setContent(html);
@@ -58,23 +43,6 @@ const generatePdf = async (html = '') => {
 	return pdfBuffer;
 };
 
-type Payload = {
-	divisionName: string;
-	formattedDate?: string;
-	matchNumber?: string;
-	fieldName?: string;
-	currentTeamName: string;
-	homeTeamName?: string;
-	awayTeamName?: string;
-	teamPlayers: {
-		number: number | null;
-		first_name: string;
-		last_name: string;
-		reserve: boolean;
-		suspended?: boolean;
-	}[];
-};
-
 export const generateMatchCardPdf = async ({
 	divisionName,
 	formattedDate,
@@ -84,7 +52,7 @@ export const generateMatchCardPdf = async ({
 	homeTeamName,
 	awayTeamName,
 	teamPlayers,
-}: Payload): Promise<Buffer> => {
+}: ExpectedBody): Promise<Buffer> => {
 	const playerRows: { number?: number | null; name?: string; reserve?: boolean; suspended?: boolean }[] = [];
 	// Need to fill up 25 player rows in the match card
 	for (let i = 0; i < 25; i++) {
@@ -398,21 +366,21 @@ export default async function handler(
 ) {
 	try {
 		console.log("Starting to parse body")
-		// const parsedBody = expectedBody.parse(req.body);
-		// console.log('Parsed body', parsedBody)
+		const parsedBody = expectedBody.parse(req.body);
+		console.log('Parsed body', parsedBody)
 		console.log('Generating pdf')
-		const pdf = await generateMatchCardPdf(req.body as Payload);
+		const pdf = await generateMatchCardPdf(parsedBody);
 		console.log('Generated pdf')
 		res.setHeader('Content-Type', 'application/pdf')
 		res.send(pdf);
 		return;
 	} catch (e) {
 		console.log('error', e)
-		// if (e instanceof ZodError) {
-		// 	res.status(400).json("Unexpected body");
-		// } else {
+		if (e instanceof ZodError) {
+			res.status(400).json("Unexpected body");
+		} else {
 			res.status(400).json("Something went wrong");
-		// }
+		}
 		return;
 	}
 }
