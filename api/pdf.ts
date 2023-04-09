c86277cfc9d3a4fa855ab6b17be2fb872d820afc
Robert Cooper <robert@robertcooper.me>
2023-04-09 12:19:15 -0400

    Stuff

--- api/pdf.ts
+++ api/pdf.ts
@@ -1,104 +1,104 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type {NextApiRequest, NextApiResponse} from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import chromium from 'chrome-aws-lambda';
 import puppeteer from 'puppeteer-core';
-import {z, ZodError} from "zod";
+import { z, ZodError } from "zod";
 
 const expectedBody = z.object({
-    divisionName: z.string(),
-    formattedDate: z.string().optional(),
-    matchNumber: z.string().optional(),
-    fieldName: z.string().optional(),
-    currentTeamName: z.string(),
-    homeTeamName: z.string().optional(),
-    awayTeamName: z.string().optional(),
-    teamPlayers: z.array(
-        z.object({
-            number: z.number().nullable(),
-            first_name: z.string(),
-            last_name: z.string(),
-            reserve: z.boolean(),
-            suspended: z.boolean().optional(),
-        })
-    )
+	divisionName: z.string(),
+	formattedDate: z.string().optional(),
+	matchNumber: z.string().optional(),
+	fieldName: z.string().optional(),
+	currentTeamName: z.string(),
+	homeTeamName: z.string().optional(),
+	awayTeamName: z.string().optional(),
+	teamPlayers: z.array(
+		z.object({
+			number: z.number().nullable(),
+			first_name: z.string(),
+			last_name: z.string(),
+			reserve: z.boolean(),
+			suspended: z.boolean().optional(),
+		})
+	)
 })
 
 type ExpectedBody = z.infer<typeof expectedBody>;
 
 const generatePdf = async (html = '') => {
-    const options = process.env.AWS_REGION
-        ? {
-            args: chromium.args,
-            executablePath: await chromium.executablePath,
-            headless: chromium.headless,
-        }
-        : {
-            args: [],
-            executablePath:
-                process.platform === 'win32'
-                    ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-                    : process.platform === 'linux'
-                        ? '/usr/bin/google-chrome'
-                        : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-        };
+	const options = process.env.AWS_REGION
+		? {
+				args: chromium.args,
+				executablePath: await chromium.executablePath,
+				headless: chromium.headless,
+		  }
+		: {
+				args: [],
+				executablePath:
+					process.platform === 'win32'
+						? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+						: process.platform === 'linux'
+						? '/usr/bin/google-chrome'
+						: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+		  };
 
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
+	const browser = await puppeteer.launch(options);
+	const page = await browser.newPage();
 
-    await page.setContent(html);
+	await page.setContent(html);
 
-    const pdfBuffer = await page.pdf({
-        format: 'letter',
-        pageRanges: '1',
-    });
+	const pdfBuffer = await page.pdf({
+		format: 'letter',
+		pageRanges: '1',
+	});
 
-    await page.close();
-    await browser.close();
+	await page.close();
+	await browser.close();
 
-    return pdfBuffer;
+	return pdfBuffer;
 };
 
 export const generateMatchCardPdf = async ({
-                                               divisionName,
-                                               formattedDate,
-                                               matchNumber,
-                                               fieldName,
-                                               currentTeamName,
-                                               homeTeamName,
-                                               awayTeamName,
-                                               teamPlayers,
-                                           }: {
-    divisionName: string;
-    formattedDate?: string;
-    matchNumber?: string;
-    fieldName?: string;
-    currentTeamName: string;
-    homeTeamName?: string;
-    awayTeamName?: string;
-    teamPlayers: {
-        number: number | null;
-        first_name: string;
-        last_name: string;
-        reserve: boolean;
-        suspended?: boolean;
-    }[];
+	divisionName,
+	formattedDate,
+	matchNumber,
+	fieldName,
+	currentTeamName,
+	homeTeamName,
+	awayTeamName,
+	teamPlayers,
+}: {
+	divisionName: string;
+	formattedDate?: string;
+	matchNumber?: string;
+	fieldName?: string;
+	currentTeamName: string;
+	homeTeamName?: string;
+	awayTeamName?: string;
+	teamPlayers: {
+    	number: number | null;
+		first_name: string;
+		last_name: string;
+		reserve: boolean;
+		suspended?: boolean;
+	}[];
 }): Promise<Buffer> => {
-    const playerRows: { number?: number | null; name?: string; reserve?: boolean; suspended?: boolean }[] = [];
-    // Need to fill up 25 player rows in the match card
-    for (let i = 0; i < 25; i++) {
-        const player = teamPlayers[i];
-        if (player) {
-            playerRows.push({
-                name: `${player.last_name}, ${player.first_name}`,
-                reserve: player.reserve,
-                number: player.number,
-                suspended: player.suspended,
-            });
-        } else {
-            playerRows.push({});
-        }
-    }
-    const html = `
+	const playerRows: { number?: number | null; name?: string; reserve?: boolean; suspended?: boolean }[] = [];
+	// Need to fill up 25 player rows in the match card
+	for (let i = 0; i < 25; i++) {
+		const player = teamPlayers[i];
+		if (player) {
+			playerRows.push({
+				name: `${player.last_name}, ${player.first_name}`,
+				reserve: player.reserve,
+        		number: player.number,
+				suspended: player.suspended,
+			});
+		} else {
+			playerRows.push({});
+		}
+	}
+	const html = `
   <html>
     <head>
       <title>Match card — ${currentTeamName}</title>
@@ -215,35 +215,202 @@ export const generateMatchCardPdf = async ({
       </style>
     </head>
     <body>
-      Hello
+      <div class="matchCard">
+      <div class="row">
+      <div class="cell title">
+        Carte de match — ${currentTeamName}
+      </div>
+    </div>
+        <div class="row">
+          <div class="horizontalPair" style="width: 50%">
+            <div class="cell fixed label" style="width: 6rem">
+              Division
+            </div>
+            <div class="cell">
+              ${divisionName}
+            </div>
+          </div>
+          <div class="horizontalPair" style="width: 50%">
+            <div class="cell fixed label" style="width: 6rem">
+              Date
+            </div>
+            <div class="cell">
+              ${formattedDate}
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="horizontalPair" style="width: 50%">
+            <div class="cell fixed label" style="width: 6rem">
+              Match
+            </div>
+            <div class="cell">
+              ${matchNumber}
+            </div>
+          </div>
+          <div class="horizontalPair" style="width: 50%">
+            <div class="cell fixed label" style="width: 6rem">
+              Terrain
+            </div>
+            <div class="cell">
+              ${fieldName}
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="horizontalPair" style="width: 70%">
+            <div class="cell fixed label" style="width: 10rem">
+              Visiteur
+            </div>
+            <div class="cell${awayTeamName === currentTeamName ? ' emphasize' : ''}">
+              ${awayTeamName}
+              ${awayTeamName === currentTeamName ? `
+              <div class="arrow">
+              <svg viewBox="0 0 65 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.0948 25.0584L0.488156 14.4518C-0.162714 13.8009 -0.162714 12.7456 0.488156 12.0947L11.0948 1.48813C11.7456 0.837254 12.8009 0.837254 13.4518 1.48813C14.1027 2.139 14.1027 3.19428 13.4518 3.84515L5.69036 11.6066L65 11.6066V14.9399L5.69036 14.9399L13.4518 22.7013C14.1027 23.3522 14.1027 24.4075 13.4518 25.0584C12.8009 25.7092 11.7456 25.7092 11.0948 25.0584Z" fill="black" />
+              </svg>
+            </div>
+              ` : ''}
+            </div>
+          </div>
+          <div class="horizontalPair" style="width: 30%">
+            <div class="cell fixed label" style="width: 8rem">
+              Pointage
+            </div>
+            <div class="cell">
+      
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="horizontalPair" style="width: 70%">
+            <div class="cell fixed label" style="width: 10rem">
+              Receveur
+            </div>
+            <div class="cell${homeTeamName === currentTeamName ? ' emphasize' : ''}">
+              ${homeTeamName}
+              ${homeTeamName === currentTeamName ? `
+              <div class="arrow">
+              <svg viewBox="0 0 65 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.0948 25.0584L0.488156 14.4518C-0.162714 13.8009 -0.162714 12.7456 0.488156 12.0947L11.0948 1.48813C11.7456 0.837254 12.8009 0.837254 13.4518 1.48813C14.1027 2.139 14.1027 3.19428 13.4518 3.84515L5.69036 11.6066L65 11.6066V14.9399L5.69036 14.9399L13.4518 22.7013C14.1027 23.3522 14.1027 24.4075 13.4518 25.0584C12.8009 25.7092 11.7456 25.7092 11.0948 25.0584Z" fill="black" />
+              </svg>
+            </div>
+              ` : ''}
+            </div>
+          </div>
+          <div class="horizontalPair" style="width: 30%">
+            <div class="cell fixed label" style="width: 8rem">
+              Pointage
+            </div>
+            <div class="cell emphasize">
+      
+            </div>
+          </div>
+        </div>
+        <div class="playerRow"> 
+          <div class="cell fixed label" style="width: 4.5rem">Présent</div>
+          <div class="cell fixed label" style="width: 3rem">No</div>
+          <div class="cell label">Nom</div>
+          <div class="cell fixed label" style="width: 3rem">R</div>
+          <div class="cell fixed label" style="width: 3rem">Buts</div>
+          <div class="cell fixed label" style="width: 3rem">A/E</div>
+        </div>
+        ${playerRows
+					.map((player) => {
+						return `
+            <div class="playerRow">
+          <div class="cell fixed" style="width: 4.5rem"></div>
+          <div class="cell fixed" style="width: 3rem">${player.number ?? ''}</div>
+          <div class="cell">${player.suspended ? '<s>' : ''}${player.name ?? ''}${player.suspended ? '</s>' : ''}${player.suspended ? '<span style="margin-left:0.5rem;font-weight:600">(suspendu)</span>' : ''}</div>
+          <div class="cell fixed" style="width: 3rem">${player.reserve ? `
+					<svg class="checkmark" viewBox="0 0 352.62 352.62">
+					<path d="M337.222 22.952c-15.912-8.568-33.66 7.956-44.064 17.748-23.867 23.256-44.063 50.184-66.708 74.664-25.092 26.928-48.348 53.856-74.052 80.173-14.688 14.688-30.6 30.6-40.392 48.96-22.032-21.421-41.004-44.677-65.484-63.648C28.774 167.385-.602 157.593.01 190.029c1.224 42.229 38.556 87.517 66.096 116.28 11.628 12.24 26.928 25.092 44.676 25.704 21.42 1.224 43.452-24.48 56.304-38.556 22.645-24.48 41.005-52.021 61.812-77.112 26.928-33.048 54.468-65.485 80.784-99.145 16.524-20.808 68.544-72.217 27.54-94.248zM26.937 187.581c-.612 0-1.224 0-2.448.611-2.448-.611-4.284-1.224-6.732-2.448 1.836-1.224 4.896-.612 9.18 1.837z" />
+          </svg>
+					` : ''}</div>
+          <div class="cell fixed" style="width: 3rem"></div>
+          <div class="cell fixed" style="width: 3rem"></div>
+        </div>
+          `;
+					})
+					.join('\n')}
+        
+        
+        <div class="refAndLegend">
+          <div class="refs">
+            <div class="row">
+              <div class="cell fixed label" style="width: 10rem">
+                Arbitre
+              </div>
+              <div class="cell">
+      
+              </div>
+            </div>
+            <div class="row">
+              <div class="cell fixed label" style="width: 10rem">
+                Arbitre Assistant
+              </div>
+              <div class="cell">
+      
+              </div>
+            </div>
+            <div class="row">
+              <div class="cell fixed label" style="width: 10rem">
+                Arbitre Assistant
+              </div>
+              <div class="cell">
+      
+              </div>
+            </div>
+          </div>
+          <div class="legend">
+            <p>A - Avertissement</p>
+            <p>E - Expulsion</p>
+            <p>R - Réserviste</p>
+          </div>
+        </div>
+        <div class="observations">
+          <div class="row">
+            <div class="cell label" style="width: 50%; justify-content: center">
+              Observations de l'arbitre
+            </div>
+            <div class="cell label" style="width: 50%; justify-content: center">
+              Observations de l'entraîneur
+            </div>
+          </div>
+          <div class="remainingHeightRow" style="display: flex; flex-grow: 1;">
+            <div class="cell"></div>
+            <div class="cell"></div>
+          </div>
+        </div>
+      </div>
     </body>
   </html>
 `;
-    return generatePdf(html);
+	return generatePdf(html);
 };
 
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<string | Buffer>
+  req: NextApiRequest,
+  res: NextApiResponse<string | 	Buffer>
 ) {
-    try {
-        console.log("Starting to parse body")
-        const parsedBody = expectedBody.parse(req.body);
-        console.log('Parsed body', parsedBody)
-        console.log('Generating pdf')
-        const pdf = await generateMatchCardPdf(parsedBody);
-        console.log('Generated pdf')
-        res.setHeader('Content-Type', 'application/pdf')
-        res.send(pdf);
-        return;
-    } catch (e) {
-        console.log('error', e)
-        if (e instanceof ZodError) {
-            res.status(400).json("Unexpected body");
-        } else {
-            res.status(400).json("Something went wrong");
-        }
-        return;
-    }
+	try {
+		console.log("Starting to parse body")
+		const parsedBody = expectedBody.parse(req.body);
+		console.log('Parsed body', parsedBody)
+		console.log('Generating pdf')
+		const pdf = await generateMatchCardPdf(parsedBody);
+		console.log('Generated pdf')
+		res.setHeader('Content-Type', 'application/pdf')
+		res.send(pdf);
+		return;
+	} catch (e) {
+		console.log('error', e)
+		if (e instanceof ZodError) {
+			res.status(400).json("Unexpected body");
+		} else {
+			res.status(400).json("Something went wrong");
+		}
+		return;
+	}
 }
