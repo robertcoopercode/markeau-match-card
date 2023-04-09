bc36413ac5fb4112eff15ff3fc4e7854f5b30dd6
Robert Cooper <robert@robertcooper.me>
2023-04-09 01:53:00 -0400

    Add strikethrough for suspended players

--- api/pdf.ts
+++ api/pdf.ts
@@ -14,10 +14,11 @@ const expectedBody = z.object({
 	awayTeamName: z.string().optional(),
 	teamPlayers: z.array(
 		z.object({
-      number: z.number().nullable(),
+			number: z.number().nullable(),
 			first_name: z.string(),
 			last_name: z.string(),
 			reserve: z.boolean(),
+			suspended: z.boolean().optional(),
 		})
 	)
 })
@@ -75,13 +76,14 @@ export const generateMatchCardPdf = async ({
 	homeTeamName?: string;
 	awayTeamName?: string;
 	teamPlayers: {
-    number: number | null;
-		first_name: string
-		last_name: string
-		reserve: boolean
+    	number: number | null;
+		first_name: string;
+		last_name: string;
+		reserve: boolean;
+		suspended?: boolean;
 	}[];
 }): Promise<Buffer> => {
-	const playerRows: { number?: number | null; name?: string; reserve?: boolean }[] = [];
+	const playerRows: { number?: number | null; name?: string; reserve?: boolean; suspended?: boolean }[] = [];
 	// Need to fill up 25 player rows in the match card
 	for (let i = 0; i < 25; i++) {
 		const player = teamPlayers[i];
@@ -89,7 +91,8 @@ export const generateMatchCardPdf = async ({
 			playerRows.push({
 				name: `${player.last_name}, ${player.first_name}`,
 				reserve: player.reserve,
-        number: player.number
+        		number: player.number,
+				suspended: player.suspended,
 			});
 		} else {
 			playerRows.push({});
@@ -318,7 +321,7 @@ export const generateMatchCardPdf = async ({
             <div class="playerRow">
           <div class="cell fixed" style="width: 4.5rem"></div>
           <div class="cell fixed" style="width: 3rem">${player.number ?? ''}</div>
-          <div class="cell">${player.name ?? ''}</div>
+          <div class="cell">${player.suspended ? '<s>' : ''}${player.name ?? ''}${player.suspended ? '</s>' : ''}${player.suspended ? '<span style="margin-left:0.5rem">(SUSPENDU)</span>' : ''}</div>
           <div class="cell fixed" style="width: 3rem">${player.reserve ? `
 					<svg class="checkmark" viewBox="0 0 352.62 352.62">
 					<path d="M337.222 22.952c-15.912-8.568-33.66 7.956-44.064 17.748-23.867 23.256-44.063 50.184-66.708 74.664-25.092 26.928-48.348 53.856-74.052 80.173-14.688 14.688-30.6 30.6-40.392 48.96-22.032-21.421-41.004-44.677-65.484-63.648C28.774 167.385-.602 157.593.01 190.029c1.224 42.229 38.556 87.517 66.096 116.28 11.628 12.24 26.928 25.092 44.676 25.704 21.42 1.224 43.452-24.48 56.304-38.556 22.645-24.48 41.005-52.021 61.812-77.112 26.928-33.048 54.468-65.485 80.784-99.145 16.524-20.808 68.544-72.217 27.54-94.248zM26.937 187.581c-.612 0-1.224 0-2.448.611-2.448-.611-4.284-1.224-6.732-2.448 1.836-1.224 4.896-.612 9.18 1.837z" />
@@ -393,7 +396,6 @@ export default async function handler(
 ) {
 	try {
 		const parsedBody = expectedBody.parse(req.body);
-    console.log('parsedBody', parsedBody)
 		const pdf = await generateMatchCardPdf(parsedBody);
 		res.setHeader('Content-Type', 'application/pdf')
 		res.send(pdf);
